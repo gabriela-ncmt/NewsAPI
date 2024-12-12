698d4477b40e0193047289571ff283ade0e815fe
Gabriela Nascimento <gabrielaf.nascimento@outlook.com>
2024-12-11 22:03:24 -0300

    implementando mappers

---
+++ API/Mappers/ViewModelToEntityMapping.cs
@@ -0,0 +1,14 @@
+﻿using API.Entities;
+using API.Entities.ViewsModels;
+using AutoMapper;
+
+namespace API.Mappers
+{
+    public class ViewModelToEntityMapping : Profile
+    {
+        public ViewModelToEntityMapping()
+        {
+            CreateMap<NewsViewModel, News> ();
+        }
+    }
+}
---
+++ API/Startup.cs
@@ -0,0 +1,38 @@
+﻿using API.Mappers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "API", Version = "v1" });
+            });
+            #region [AutoMapper]
+            services.AddAutoMapper(typeof(EntityToViewModelMapping), typeof(ViewModelToEntityMapping));
+            #endregion
+        }
+
+        public void Configure(IApplicationBuilder app,  IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+            }
+        }
+    }
+}
