073752b830288574d619bca9d1ef078b141156ec
Gabriela Nascimento <gabrielaf.nascimento@outlook.com>
2024-12-11 21:24:50 -0300

    implementando ViewsModel

---
+++ API/Entities/ViewsModels/NewsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using API.Entities.Enums;
+
+namespace API.Entities.ViewsModels
+{
+    public class NewsViewModel
+    {
+        public string Id { get; set; }
+        public string Hat {  get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Author { get; set; }
+        public string Img { get; set; }
+        public string Link { get; set; }
+        public Status Status { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
